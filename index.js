6ef806f107e26fb129b2e13a024da27eb216182d
Deeksha-555999 <dhimandeeksha92@gmail.com>
2026-05-06 13:56:09 +0530

    second commit

--- index.js
+++ index.js
@@ -3,19 +3,24 @@ const express = require('express');
 const userRouter = require("./routes/user");
 const connectDB = require("./mongodb.js");
 const bodyParser = require('body-parser');
-
+const cookieParser = require('cookie-parser');
+const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 connectDB();
 
 const app = express()
+app.use(cookieParser());
+
+app.use( checkForAuthenticationCookie("token"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const PORT = 3001; 
 
 app.set("view engine", 'ejs' )
 app.set( "views", path.resolve("./views"))
+
  
 app.get("/", (req, res) => {
-    res.render("home");
+    res.render("home" ,{user: req.user});
 }) 
 
 app.use('/user', userRouter);
