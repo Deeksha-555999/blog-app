7e6b6ec17a016b153257289b6a21f8f172be99ba
Deeksha-555999 <dhimandeeksha92@gmail.com>
2026-05-07 13:53:11 +0530

    third commit

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const userRouter = require("./routes/user");
 const connectDB = require("./mongodb.js");
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const blogRouter = require("./routes/blog");
-const { checkForAuthenticationCookie } = require("./middlewares/authentication");
+const {
+  checkForAuthenticationCookie,
+} = require("./middlewares/authentication");
+const Blog = require("./models/blog");
 connectDB();
 
-const app = express()
+const app = express();
 app.use(cookieParser());
 
-app.use( checkForAuthenticationCookie("token"));
+app.use(checkForAuthenticationCookie("token"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const PORT = 3001; 
+const PORT = 3001;
 
-app.set("view engine", 'ejs' )
-app.set( "views", path.resolve("./views"))
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
- 
-app.get("/", (req, res) => {
-    res.render("home" ,{user: req.user});
-}) 
+app.get("/", async(req, res) => { 
+     const allBlogs = await Blog.find({})
+  res.render("home", { user: req.user, blogs: allBlogs });
+});
 
-app.use('/user', userRouter);
-app.use('/blog', blogRouter);
+app.use("/user", userRouter);
+app.use("/blog", blogRouter);
 
-
-
-app.listen(PORT , ()=> 
-    console.log(`Server Started at PORT: ${PORT}`)
-)
-
+app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`));
